e906703c964d646d4b65d1ef05e0f019e5ceb253
Sayali Kadam <22bcs122@iiitdmj.ac.in>
2023-12-25 00:05:06 +0530

    connected to database in XAMPP

--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine, text
-
-engine = create_engine("mysql+mysqlconnector://root:''@localhost/jobs")
-
-conn = engine.connect()
-
-
-result = conn.execute(text("select * from job_info"))
-print(result.fetchall)
